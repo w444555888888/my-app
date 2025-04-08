76e3d4fcb8ea870773b5fb78fff71d643c7b980a
mikey <mikeyu@al88tw.com>
2025-04-08 17:46:53 +0800

    header不帶token，需要驗證身分的時候後端帶cookie

--- client/src/utils/apiService.js
+++ client/src/utils/apiService.js
@@ -1,19 +1,8 @@
 import axios from 'axios'
 
+
 const API_BASE_URL = 'http://localhost:5000/api/v1'
 
-axios.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token')
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`
-        }
-        return config
-    },
-    (error) => {
-        return Promise.reject(error)
-    }
-)
 
 export const request = async (method, endpoint, data = {}, setLoading = () => {}) => {
     if (setLoading) setLoading(true)
