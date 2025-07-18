f258f538ff9f687882fcfc18c5fdfd125639ff49
mikey <mikeyu@al88tw.com>
2025-07-18 14:58:52 +0800

    調整後端verifyCaptcha錯誤提示

--- api/RoutesController/captcha.js
+++ api/RoutesController/captcha.js
@@ -6,9 +6,9 @@ import { errorMessage } from "../errorMessage.js";
 export const initCaptcha = (req, res, next) => {
   try {
     const x = Math.floor(Math.random() * 80 + 100);
-    const token = jwt.sign({ x }, process.env.JWT, { expiresIn: "5m" });
+    const token = jwt.sign({ x }, process.env.JWT, { expiresIn: "1m" });
 
-    sendResponse(res, 200, {  token, targetX: x }, "滑塊驗證初始化");
+    sendResponse(res, 200, { token, targetX: x }, "滑塊驗證初始化");
   } catch (err) {
     return next(errorMessage(500, "滑塊初始化失敗", err));
   }
@@ -20,16 +20,19 @@ export const verifyCaptcha = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT);
     const correctX = decoded.x;
-    const pieceSize = 50;  // 前端拼圖寬度一致
     const tolerance = 6;   // 允許誤差範圍
-      const isPassed = Math.abs(userX - correctX) < tolerance;
-    
+    const isPassed = Math.abs(userX - correctX) < tolerance;
     if (!isPassed) {
       return next(errorMessage(400, "滑塊驗證未通過"));
     }
 
     sendResponse(res, 200, { success: true }, "滑塊驗證成功");
   } catch (err) {
+    if (err.name === "TokenExpiredError") {
+      return next(errorMessage(401, "驗證失敗：Token 已過期"));
+    } else if (err.name === "JsonWebTokenError") {
+      return next(errorMessage(401, "驗證失敗：無效的 Token"));
+    }
     return next(errorMessage(403, "Token 驗證失敗", err));
   }
 };
